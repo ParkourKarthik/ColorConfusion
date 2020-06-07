70d23f48a793cf4698ffc677696d16293e606b99
parkourkarthik <parkourkarthik@gmail.com>
2020-06-07 13:27:16 +0530

    use of Scope function, add comments on concepts usage

--- app/src/main/java/com/example/android/colorconfusion/MainActivity.kt
+++ app/src/main/java/com/example/android/colorconfusion/MainActivity.kt
@@ -15,13 +15,21 @@ class MainActivity : AppCompatActivity() {
     private var counts: Int = 0
     private var score: Int = 0
     private var expectedAnswer: Int = 0
+    private var colorMaps: List<ColorMap> = listOf(
+        ColorMap(Color.RED, Color::RED.name),
+        ColorMap(Color.BLUE, Color::BLUE.name),
+        ColorMap(Color.GREEN, Color::GREEN.name),
+        ColorMap(Color.YELLOW, Color::YELLOW.name),
+        ColorMap(Color.GRAY, Color::GRAY.name),
+        ColorMap(Color.BLACK, Color::BLACK.name)
+    )
+    // Use of late initialization
     private lateinit var lBtn: Button
     private lateinit var rBtn: Button
     private lateinit var scoreLabel: TextView
     private lateinit var countLabel: TextView
     private lateinit var textQuery: TextView
     private lateinit var list: List<Button>
-    private lateinit var colorMaps: List<ColorMap>
     private lateinit var refreshBtn: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,14 +50,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupGame() {
-        colorMaps = listOf(
-            ColorMap(Color.RED, Color::RED.name),
-            ColorMap(Color.BLUE, Color::BLUE.name),
-            ColorMap(Color.GREEN, Color::GREEN.name),
-            ColorMap(Color.YELLOW, Color::YELLOW.name),
-            ColorMap(Color.GRAY, Color::GRAY.name),
-            ColorMap(Color.BLACK, Color::BLACK.name)
-        )
         for (btn in list) {
             btn.setOnClickListener {
                 if ((it.background as ColorDrawable).color == ColorDrawable(expectedAnswer).color) {
@@ -60,7 +60,7 @@ class MainActivity : AppCompatActivity() {
                 resetGameColors()
             }
         }
-        refreshBtn.setOnClickListener{
+        refreshBtn.setOnClickListener {
             resetGameColors()
             resetScoreBar()
         }
@@ -83,17 +83,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setWordAndColor(involvedColors: List<ColorMap>) {
-        val wrd: TextView = findViewById(R.id.textColorWord)
-        wrd.text = involvedColors[0].name // word
-        wrd.setTextColor(involvedColors[1].value) // color
+        // Use of Scope function
+        findViewById<TextView>(R.id.textColorWord).let {
+            it.text = involvedColors[0].name // word
+            it.setTextColor(involvedColors[1].value) // color
+        }
     }
 
     //param with ("word", "color")
     private fun setQueryText(involvedColors: List<ColorMap>) {
         val queries = arrayOf("Word?", "Color?")
-        val currentQuery = queries.random()
-        textQuery.text = currentQuery
-        expectedAnswer = involvedColors[queries.indexOf(currentQuery)].value
+        // Use of Scope function
+        queries.random().let {
+            textQuery.text = it
+            expectedAnswer = involvedColors[queries.indexOf(it)].value
+        }
     }
 
     private fun setButtonColors(involvedColors: List<ColorMap>) {
@@ -104,5 +108,5 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-
+// Use of data class
 data class ColorMap(val value: Int, val name: String)
